5512f27a4ed8cd56370e9335a486481659ce55fa
RomGutman <rom.gutman1@gmail.com>
2018-01-07 18:24:57 +0200

    changed names

--- struct_perceptron.py
+++ struct_perceptron.py
@@ -34,7 +34,7 @@ class StructPerceptron:
         self.gold_tree = None
         self.inference_mode(mode)
         # constant which represent the 'root' node in the data
-        self._ROOT = int(0)
+        self._ROOT = 0
 
     def inference_mode(self, mode='train'):
         """
