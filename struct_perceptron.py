4a114e153bf91fefca40681346942a40bfbb777a
RomGutman <rom.gutman1@gmail.com>
2018-01-07 12:36:29 +0200

    added methods and fixed bugs:  - added constant definition of 'root' node  - added exception handling (on assert)  - extracted from the perceptron method the mst calculation to a method, to allow a more generic use by evaluation class  - added handling of different inference modes (test, train, comp)  - fixed wrong use of index in creation of full graph

--- struct_perceptron.py
+++ struct_perceptron.py
@@ -8,73 +8,117 @@ class StructPerceptron:
     this class implements the structured perceptron, calculates the best weights
     using Chu-Liu-Edmonds algorithm to find the Maximum Spanning Tree
     """
-    # todo: set a score function to send to chu-liu
-    # todo: implement the pseudo-code
-    # todo: change 'root' tag to 0
-    # todo: try and catch
-    # todo: extract to external function chu-liu uses (lines 39:46), calculate_mst()
-    def __init__(self, gold_tree, model):
+    # todo: set of nodes need to be set one time only (at full graph creation)
+    # todo: create the full graph on creation (with default of 'train')
+    def __init__(self, model):
         """
         :param model: an object of the model, which will create for a given edge its feature vector
-        :param gold_tree: a representation of the gold dependency tree, which is represented by a list of tuples
-        in a form of (x_i,y_i) where x_i is the head node and y_i the target node
+        :type model: parser_model.ParserModel
         """
         self.model = model
         self.feature_vec_len = model.feature_vec_len
-        self.gold_tree = model.train_gold_tree
+        self.train_gold_tree = model.train_gold_tree
+        self.test_gold_tree = model.test_gold_tree
+        # todo: make sure this implemented in Reut's code
+        self.comp_gold_tree = model.comp_gold_tree
+        # the feature vector of a complete sentence in the training
         self.features_vector_train = model.features_vector_train
         self.weight_matrix = []
         self.current_weight_vec_iter = 0
-        self.current_weight_vec = np.empty(self.feature_vec_len)
+        self.current_weight_vec = np.zeros(shape=self.feature_vec_len)
         self.full_graph = {}
         self.current_sentence = 0
+        # mode of the Class
+        self._mode = 'train'
+        self.gold_tree = self.train_gold_tree
+        # constant which represent the 'root' node in the data
+        self._ROOT = 0
+
+    def inference_mode(self, mode='train'):
+        """
+        based on whether we use this class for train or test,
+        this method sets the gold tree source and the mode of using the model functions,
+        and than creates a new full_graph dictionary out of the relevant gold tree
+
+        :param str mode: indicates class mode:
+         * train mode ('train')
+         * test mode ('test')
+         * competition mode ('comp')
+        :return: None
+        """
+        if mode == 'train':
+            self._mode = mode
+            self.gold_tree = self.train_gold_tree
+        elif mode == 'test':
+            self._mode = mode
+            self.gold_tree = self.test_gold_tree
+        else:
+            self._mode = mode
+            self.gold_tree = self.comp_gold_tree
+        self.create_full_graph()
 
     def perceptron(self, num_of_iter):
         """
         this method implements the pseudo-code of the perceptron
+
         :param num_of_iter: N from the pseudo-code
-        :return:
+        :return: the final weight vector
         """
         for i in range(num_of_iter):
             for t in range(len(self.gold_tree)):
                 self.current_sentence = t
-                pred_tree = self.full_graph.get(t)
-                if pred_tree is None:
-                    self.create_full_graph()
-                    pred_tree = self.full_graph.get(t)
-                digraph = Digraph(pred_tree, get_score=self.edge_score)
-                new_graph = digraph.mst()
-                pred_tree = new_graph.successors
-                assert self.check_valid_tree(pred_tree, t)
-                if not self.identical_dependency_tree(pred_tree, self.gold_tree[t]):
-                    # todo: collab with Reut on the exact functions
-                    curr_feature_vec = self.features_vector_train[t]  # todo: Reut changed it to the correct dictionary
-                    new_feature_vec = self.model.create_global_feature_vector(pred_tree, t, 'train')
-                    new_weight_vec = np.empty(self.feature_vec_len)  # todo: check if this is faster
-                    new_weight_vec = self.current_weight_vec + curr_feature_vec - new_feature_vec
-                    self.weight_matrix.append(new_weight_vec)
-                    self.current_weight_vec_iter += 1
-                    self.current_weight_vec = new_weight_vec
+                try:
+                    pred_tree = self.calculate_mst(t)
+                except AssertionError as err:
+                    pred_tree = err.args
+                    print("The algorithm returned a bad tree, update is skipped. \n tree: {}".format(pred_tree))
+                else:
+                    if not self.identical_dependency_tree(pred_tree, self.gold_tree[t]):
+                        curr_feature_vec = self.features_vector_train[t]
+                        new_feature_vec = self.model.create_global_feature_vector(pred_tree, t, data=self._mode)
+                        new_weight_vec = np.zeros(shape=self.feature_vec_len)
+                        new_weight_vec = self.current_weight_vec + curr_feature_vec - new_feature_vec
+                        self.weight_matrix.append(new_weight_vec)
+                        self.current_weight_vec_iter += 1
+                        self.current_weight_vec = new_weight_vec
         return self.current_weight_vec
+
+    def calculate_mst(self, t):
+        """
+
+        :param int t: the index of the sentence
+        :return:
+        :raise AssertionError: with argument of the defected predicated tree
+        """
+        pred_tree = self.full_graph.get(t)
+        if pred_tree is None:
+            self.create_full_graph()
+            pred_tree = self.full_graph.get(t)
+        digraph = Digraph(pred_tree, get_score=self.edge_score)
+        new_graph = digraph.mst()
+        pred_tree = new_graph.successors
+        assert self.check_valid_tree(pred_tree, t), pred_tree
+        return pred_tree
 
     def create_full_graph(self):
         """
         this method will create for a given gold dependency tree,
         a fully connected graph from each sentence of it
+
         :return: a fully connected tree
         """
-        for idx, sentence in enumerate(self.gold_tree.values()):
+        for idx, sentence in self.gold_tree.items():
             set_of_nodes = set()
             for source, targets in sentence.items():
                 set_of_nodes.add(source)
                 set_of_nodes.union(set(targets))
-            if 'root' in set_of_nodes:
-                set_of_nodes.remove('root')
+            if self._ROOT in set_of_nodes:
+                set_of_nodes.remove(self._ROOT)
             graph = {}
             for node in set_of_nodes:
                 targets = list(set_of_nodes.difference({node}))
                 graph.update({node: targets})
-            graph.update({'root': list(set_of_nodes)})
+            graph.update({self._ROOT: list(set_of_nodes)})
             self.full_graph.update({idx: graph})
         return
 
@@ -82,16 +126,18 @@ class StructPerceptron:
         """
         this method return a score of likelihood , for a pair of source and target
         s(source,target) = weight_vec * feature_vec(source, target)
+
         :param source: a source node
         :param target: a target node
         :return: score value
         """
-        feature_vec = self.model.get_local_feature_vec(self.current_sentence, source, target, 'train')
+        feature_vec = self.model.get_local_feature_vec(self.current_sentence, source, target, data=self._mode)
         return self.current_weight_vec.dot(feature_vec)
 
     def identical_dependency_tree(self, pred_tree, gold_tree):
         """
         this method evaluate whether two dependency trees are identical
+
         :param pred_tree:
         :param gold_tree:
         :return:
@@ -106,15 +152,22 @@ class StructPerceptron:
         return True
 
     def check_valid_tree(self, pred_tree, t):
+        """
+        check weather the tree returned from the Chu-Liu-Edmonds algorithm is a valid tree
+
+        :param pred_tree:
+        :param t:
+        :return:
+        """
         gold_tree = self.gold_tree[t]
-        if len(pred_tree['root']) != 1:
+        if len(pred_tree[self._ROOT]) != 1:
             return False
         set_of_nodes = set()
         for source, targets in gold_tree.items():
             set_of_nodes.add(source)
             set_of_nodes.union(set(targets))
-        if 'root' in set_of_nodes:
-            set_of_nodes.remove('root')
+        if self._ROOT in set_of_nodes:
+            set_of_nodes.remove(self._ROOT)
         for node in set_of_nodes:
             if sum(node in targets for targets in pred_tree.values()) != 1:
                 return False
